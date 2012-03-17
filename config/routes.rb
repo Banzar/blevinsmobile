5432023127421c74bb5bd2495bbddc3669b48aa4
David Ledford <dcledford@ymail.com>
2012-03-17 02:33:39 -0400

    contact info updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blevinsmobile::Application.routes.draw do
 	
   get "repairs" => "services#repairs", :as => "repairs"
+	get "contact" => "services#contact", :as => "contact"
 
 	root :to => "services#index"
 
